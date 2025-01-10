512e47da23661792e0459b26f7f28892db31a21a
Jer Boon <Jeremy.Boon@orx.org>
2025-01-10 11:32:00 +0000

    13 Part 2

--- 13.R
+++ 13.R
@@ -33,3 +33,35 @@ for (i in 1:nrow(df)) {
 }
 tot
 
+## Part 2 -------------
+
+# The method used in part 1 solve() function is not going to work - the loop will take a VERY long time!
+
+solve2 <- function(a.x, a.y, b.x, b.y, prize.x, prize.y, offset=0) {
+  
+  # It's the intersection of two slopes!
+  # This equation was worked out on paper, sorry..
+  
+  a <- (offset+prize.x)/b.x - (offset+prize.y)/b.y
+  a <- round(a / (a.x/b.x - a.y/b.y),0)
+  b <- round(((prize.x + offset) -a*a.x) / b.x,0)
+
+  # Look for rounding errors in the big equation
+  if (a * a.x + b * b.x != prize.x + offset) return(0)
+  if (a * a.y + b * b.y != prize.y + offset) return(0)
+  return(a*3+b)
+}
+
+
+tot <- 0
+for (i in 1:nrow(df)) {
+  tot <- tot + solve2(df[i,1],df[i,2],df[i,3],df[i,4],df[i,5],df[i,6], offset=10000000000000)
+  print(".")
+}
+tot
+
+i = 4
+solve2(df[i,1],df[i,2],df[i,3],df[i,4],df[i,5],df[i,6], offset=10000000000000)
+
+
+
